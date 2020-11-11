d03a42d23609c3055dd36f53b37779b1dc6dbf8b
paulrumyancev <ds>
2020-11-11 16:21:47 +0200

    Code cleanup

--- Assets/Scripts/StateBehaviours/AnimatorStateChangeNotifications.cs
+++ Assets/Scripts/StateBehaviours/AnimatorStateChangeNotifications.cs
@@ -3,28 +3,23 @@ using UnityEngine;
 
 namespace SmbExample.StateBehaviours
 {
-    public class AnimatorStateChangeNotifications : StateMachineBehaviour
+
+public class AnimatorStateChangeNotifications : StateMachineBehaviour
+{
+    public event Action<int> StateEnter;
+
+    public event Action<int> StateExit;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        #region Events
-
-        public event Action<int> StateEnter;
-
-        public event Action<int> StateExit;
-
-        #endregion
-
-        #region Methods
-
-        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            StateEnter.Invoke(stateInfo.shortNameHash);
-        }
-
-        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            StateExit?.Invoke(stateInfo.shortNameHash);
-        }
-
-        #endregion
+        StateEnter.Invoke(stateInfo.shortNameHash);
     }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        StateExit?.Invoke(stateInfo.shortNameHash);
+    }
+}
+
+
 }
