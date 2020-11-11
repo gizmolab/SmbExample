25f10e20cc7dfce5a37fe230c069d1e555d28b25
paulrumyancev <ds>
2020-11-11 12:53:06 +0200

    Initial commit

---
+++ Assets/Scripts/Logic/StateChangesHandler.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using SmbExample.StateBehaviours;
+
+namespace SmbExample.Logic
+{
+    [RequireComponent(typeof(Animator))]
+    public class StateChangesHandler : MonoBehaviour
+    {
+        // Name of the state we want to get notifications from
+        private const string STATE_NAME = "ExampleState";
+
+        private int _stateNameHash;
+
+        private void Awake()
+        {
+            _stateNameHash = Animator.StringToHash(STATE_NAME);
+
+            var animator = GetComponent<Animator>();
+            var notifiers = animator.GetBehaviours<AnimatorStateChangeNotifications>();
+            foreach (var notifier in notifiers)
+            {
+                notifier.StateEnter += OnStateEnter;
+                notifier.StateExit += OnStateExit;
+            }
+        }
+
+        private void OnStateEnter(int stateNameHash)
+        {
+            if (_stateNameHash != stateNameHash)
+            {
+                return;
+            }
+            OnExampleStateEnter();
+        }
+
+        private void OnStateExit(int stateNameHash)
+        {
+            if (_stateNameHash != stateNameHash)
+            {
+                return;
+            }
+            OnExampleStateExit();
+        }
+
+        private void OnExampleStateEnter()
+        {
+            Debug.Log("OnExampleStateEnter");
+        }
+
+        private void OnExampleStateExit()
+        {
+            Debug.Log("OnExampleStateExit");
+        }
+    }
+}
---
+++ Assets/Scripts/StateBehaviours/AnimatorStateChangeNotifications.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace SmbExample.StateBehaviours
+{
+    public class AnimatorStateChangeNotifications : StateMachineBehaviour
+    {
+        #region Events
+
+        public event Action<int> StateEnter;
+
+        public event Action<int> StateExit;
+
+        #endregion
+
+        #region Methods
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            StateEnter.Invoke(stateInfo.shortNameHash);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            StateExit?.Invoke(stateInfo.shortNameHash);
+        }
+
+        #endregion
+    }
+}
