689af8bb5dcf2b1b3da79bb98a1d94084b2cf0da
paulrumyancev <ds>
2020-11-11 16:43:32 +0200

    Formatting fix

--- Assets/Scripts/StateBehaviours/AnimatorStateChangeNotifications.cs
+++ Assets/Scripts/StateBehaviours/AnimatorStateChangeNotifications.cs
@@ -3,23 +3,20 @@ using UnityEngine;
 
 namespace SmbExample.StateBehaviours
 {
-
-public class AnimatorStateChangeNotifications : StateMachineBehaviour
-{
-    public event Action<int> StateEnter;
-
-    public event Action<int> StateExit;
-
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public class AnimatorStateChangeNotifications : StateMachineBehaviour
     {
-        StateEnter.Invoke(stateInfo.shortNameHash);
+        public event Action<int> StateEnter;
+
+        public event Action<int> StateExit;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            StateEnter.Invoke(stateInfo.shortNameHash);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            StateExit?.Invoke(stateInfo.shortNameHash);
+        }
     }
-
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        StateExit?.Invoke(stateInfo.shortNameHash);
-    }
-}
-
-
 }
